cd7d6824a916c5cf16e5f24abe256a56be9df960
EpicFailure-afk <as3704921@gmail.com>
2026-09-13 20:32:50 +0300

    extension method

--- Day-29 (Linq-1)/IntroTolinq_2/Extensions.cs
+++ Day-29 (Linq-1)/IntroTolinq_2/Extensions.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace IntroTolinq_2 {
-  internal static class extentions_2 {
-    // only edit on the old filter function is ((((((( this ))))))
+  internal static class Extensions {
+    // only edit on the old filter function is ((((((( this )))))))
     public static void Filter(this List<int> numbers, Predicate<int> check) {
       foreach(int number in numbers) {
         if (check(number)) {
