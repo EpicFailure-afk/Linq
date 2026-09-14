cd403e2f2bfef42e359cd5b6c26242fc76f91136
EpicFailure-afk <as3704921@gmail.com>
2026-09-14 21:21:01 +0300

    Sequence
    IEnumerable, sub and coreoutine, eager and deferred exec

---
+++ Day-30 (Linq-2)/Linq-2/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linq_2 {
+  internal static class Extensions {
+    // eager execution
+    
+    public static IEnumerable<T> Filter<T>(this IEnumerable<T> list, Predicate<T> predicate) {
+      List<T> result = new List<T>();
+
+      foreach (var item in list) {
+        if (predicate(item)) {
+          result.Add(item);
+        }
+      }
+
+      return result;
+    }
+    
+
+    // defeared execution with execusion method Filter
+    public static IEnumerable<T> Filter2<T>(this IEnumerable<T> list, Predicate<T> predicate) {
+
+      foreach (var item in list) {
+        if (predicate(item)) {
+          // result.Add(item);
+          yield return item;  // bc of yield return, the returnd type must be --> IEnumerable 
+        }
+      }
+
+      
+    }
+  }
+}
---
+++ Day-30 (Linq-2)/Linq-2/Program.cs
@@ -0,0 +1,47 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Linq_2 {
+  internal class Program {
+
+    static IEnumerable<int> Sequence() {
+      //List<int> list = new List<int>() { 1, 2, 3 };
+      //return list;  // here func is called only one time and return only one time
+      //                    that means eager execution 
+
+      yield return 1; // yield makes the execution deferred till iteration 
+
+      // subroutine ---> is the normal function
+      // yield make the function ---> coroutine: continue from the last point
+      //                                instead of returning what the fun return as one package 
+      //                              that mean defeard execution
+
+      yield return 2;
+
+      yield return 3;
+    }
+    static void Main(string[] args) {
+      // part one
+      
+      IEnumerable<int> list = Sequence(); // list is ref that point at the created obj
+      // list in the subroutine concept is the carreing the data that implemented by Sequence()
+      // but lit at the coroutine concept only caries the function itself without implementation yet
+
+
+      foreach (int item in list) {
+        Console.WriteLine(item);
+      }
+
+      Console.WriteLine("--------------------------------------------");
+
+      // part two, with yield return
+      var list2 = new List<int>() { 1, 2, 3, -4, 5, -6, -7, 8, 9 };
+      IEnumerable<int> newList = list2.Filter2(x => x % 2 == 0);
+      //newList does not contain the filtered data yet.
+      //It references an IEnumerable that represents
+      //the deferred filtering operation.
+      foreach (int item in newList) {
+        Console.WriteLine(item);
+      }
+    }
+  }
+}
